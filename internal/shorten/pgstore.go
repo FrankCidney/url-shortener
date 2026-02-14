1a8b289330a7b11618ed821caeaff90cede3203c
frawuor <franciscidneyawuor@gmail.com>
2026-02-14 14:14:40 +0300

    feat: add postgres store, update store interface

---
+++ internal/shorten/pgstore.go
@@ -0,0 +1,51 @@
+package shorten
+
+import "database/sql"
+
+type PGStore struct {
+	db *sql.DB
+}
+
+func NewPGStore(db *sql.DB) *PGStore {
+	return &PGStore{db: db}
+}
+
+func (store *PGStore) Save(link ShortLink) error {
+	_, err := store.db.Exec(`
+	INSERT INTO links (short_id, original_url, hits, created_at)
+	VALUES ($1, $2, $3, NOW())
+	`, link.ID, link.URL, link.Hits)
+	
+	return err
+}
+
+func (store *PGStore) Get(id string) (ShortLink, error) {
+	var link ShortLink
+
+	err := store.db.QueryRow(`
+	SELECT short_id, original_url, hits, created_at 
+	FROM links 
+	WHERE short_id = $1
+	`, id).Scan(
+		&link.ID,
+		&link.URL,
+		&link.Hits,
+		&link.CreatedAt,
+	)
+
+	if err != nil {
+		return ShortLink{}, err
+	}
+
+	return link, nil
+}
+
+func (store *PGStore) IncrementHits(id string) error {
+	_, err := store.db.Exec(`
+	UPDATE links (hits)
+	SET hits = hits + 1
+	WHERE short_id = $1
+	`, id)
+
+	return err
+}
--- internal/shorten/repository.go
+++ internal/shorten/repository.go
@@ -1,71 +1,8 @@
 package shorten
 
-import "sync"
-
-// type Store interface {
-// 	Save(id string, url string) error
-// 	Get(id string) (string, bool)
-// 	List() map[string]string
-// }
-
 type Store interface {
 	Save(link ShortLink) error
-	Get(id string) (ShortLink, bool)
-	List() []ShortLink
-	IncrementHits(id string)
-}
-
-type MemStore struct {
-	mu sync.RWMutex
-	data map[string]ShortLink
-}
-
-func NewMemStore() *MemStore {
-	return &MemStore{
-		data: make(map[string]ShortLink),
-	}
-}
-
-func (store *MemStore) Save(link ShortLink) error {
-	store.mu.Lock()
-	defer store.mu.Unlock()
-	
-	store.data[link.ID] = link
-
-	return nil
-}
-
-func (store *MemStore) Get(id string) (ShortLink, bool) {
-	store.mu.RLock()
-	defer store.mu.RUnlock()
-
-	value, exists := store.data[id]
-	return value, exists
-}
-
-func (store *MemStore) List() []ShortLink {
-	store.mu.RLock()
-	defer store.mu.RUnlock()
-
-	// return a snapshot, not the internal map
-	copy := make([]ShortLink, 0, len(store.data))
-
-	for _, v := range store.data {
-		copy = append(copy, v)
-	}
-
-	return copy
-}
-
-func (store *MemStore) IncrementHits(id string) {
-	store.mu.Lock()
-	defer store.mu.Unlock()
-
-	link, ok := store.data[id]
-	if !ok {
-		return
-	}
-
-	link.Hits++
-	store.data[id] = link
+	Get(id string) (ShortLink, error)
+	// List() []ShortLink
+	IncrementHits(id string) error
 }
